9fbfec61f0532532cfd3a86813eda9669e96d667
Evan Summers <evan.summers@gmail.com>
2017-02-09 23:57:44 +0000

    initial

--- test/curl.sh
+++ test/curl.sh
@@ -5,4 +5,6 @@ set -u -x
   curl 'http://localhost:8888/maps/api/geocode/json' -G --data-urlencode 'address=14 Grays Inn Road, Chancery Lane, London'
   sleep 1
   redis-cli keys cache-geo-proxy:* 
+  redis-cli hkeys cache-geo-proxy:url:h
+  redis-cli hgetall cache-geo-proxy:url:h
 
