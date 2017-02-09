b64ead2ff54a9a797fbacafa8a6c0b0ebbadb6d7
Evan Summers <evan.summers@gmail.com>
2017-02-09 18:45:08 +0000

    initial

--- development/run.sh
+++ development/run.sh
@@ -1,5 +1,9 @@
 
+set -u -e
+
 mkdir -p tmp
+
+redis-cli keys cache-geo-proxy:* | xargs -n 1 redis-cli del
 
 echo '
 redis-cli keys cache-geo-proxy:* | head
@@ -9,8 +13,13 @@ cat tmp/keys.sh
 ls -l tmp/keys.sh
 
 (
+  echo 'client sleep'
   sleep 1
-  curl -X -d '{address: "10 Downing Street, London"}' http://localhost:8888/maps/api/geocode/json
+  echo 'client curl'
+  curl -X POST -d '{"address": "20 Falstaff Close, Eynsham OX29 4QA"}' \
+    --header 'Content-Type: application/json' \
+    http://localhost:8888/maps/api/geocode/json
 ) &
 
+echo 'start server'
 apiKey=$MAPS_API_KEY node --harmony lib/index.js
